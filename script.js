f05a5f6e93f691a8e3b1ef5d23c5d326daf122fe
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-18 12:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function toCase(text) {
   // write your code here
+if(!text){
+	return '-';
+}
+
+	const lowercase=text.toLowerCase();
+	const uppercase=text.toUpperCase();
+	return `${lowercase}-${uppercase}`;
 }
 
 // DO not change the code below
